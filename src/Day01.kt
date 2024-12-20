f095a80948ab0f5fb8abe34c64bfdb7260876030
Cedrik Beauregard <beauregardced@gmail.com>
2024-12-19 21:52:45 -0500

    Starting 2024 instead. Day 1 part 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,41 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val test = input.map { line ->
+            val first = line.substringBefore(" ").toInt()
+            val second = line.substringAfterLast(" ").toInt()
+            first to second
+        }.unzip()
+
+        val left = test.first.sorted()
+        val right = test.second.sorted()
+
+        println(left.size)
+        println(right.size)
+
+        var listDistance = 0
+        for (i in left.indices) {
+            listDistance += abs(left[i] - right[i])
+        }
+
+        // Autre solution plus fp
+//         val result = left.sorted().zip(right.sorted()).sumOf { (first, second) ->
+//             abs(first - second)
+//         }
+
+        return listDistance
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
+
+
+
     part1(input).println()
-    part2(input).println()
+//    part2(input)
 }
--- src/Day01_solution.kt
+++
@@ -1,2 +0,0 @@
-class Day01_solution {
-}
